f88003494d93d5d04e4d5365799ee80bba8d8f62
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-18 23:36:06 +0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hina Khan on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

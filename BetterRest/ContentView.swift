3977309a3a7e9b4dc907f8fd553c3db76faafedc
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-25 00:25:51 +0500

    Connecti SwiftUI to Core ML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
 
     var body: some View {
         NavigationStack {
@@ -33,6 +36,10 @@ struct ContentView: View {
             }.navigationTitle("BetterRest")
                 .toolbar {
                     Button("Calculate", action: calculateBedtime)
+                }.alert(alertTitle, isPresented: $showingAlert) {
+                    Button("OK") { }
+                } message: {
+                    Text(alertMessage)
                 }
         }
     }
@@ -49,9 +56,13 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
 
             let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
 
             
         }catch{
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
         showingAlert = true
     }

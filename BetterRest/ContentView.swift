b6017d75102898c91439ca897588171683b806a4
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-24 22:15:12 +0500

    Build a basic layout for betterRest app

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,18 +10,33 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
 
     var body: some View {
-        VStack{
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12,step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp,in: Date.now...
-//                       ,displayedComponents: .hourAndMinute
-            ).labelsHidden()
-            
-            Text(Date.now, format: .dateTime.hour().minute())
-            Text(Date.now, format: .dateTime.day().month().year())
-            Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationStack {
+            VStack{
+                
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+            }.navigationTitle("BetterRest")
+                .toolbar {
+                    Button("Calculate", action: calculateBedtime)
+                }
         }
+    }
+    
+    func calculateBedtime() {
     }
     
     func exampleDates() {
@@ -42,7 +57,6 @@ struct ContentView: View {
         let components2 = Calendar.current.dateComponents([.hour, .minute], from: .now)
         let hour = components2.hour ?? 0
         let minute = components2.minute ?? 0
-        
     }
 }
 

77d6b5f875cffa3bd80e9e8a591f618b3fd66b58
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-25 00:25:06 +0500

    predict sleeptime

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -37,6 +37,22 @@ struct ContentView: View {
     }
     
     func calculateBedtime() {
+        do{
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            
+        }catch{
+        }
+        showingAlert = true
     }
     
     func exampleDates() {

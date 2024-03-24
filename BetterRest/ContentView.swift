9529169866a90e64bb0241846134b6f6f791b0e5
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-24 21:53:23 +0500

    work with dates

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -17,15 +17,32 @@ struct ContentView: View {
             DatePicker("Please enter a date", selection: $wakeUp,in: Date.now...
 //                       ,displayedComponents: .hourAndMinute
             ).labelsHidden()
+            
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
     }
     
     func exampleDates() {
+//
         let now = Date.now
         // create a second Date instance set to one day in seconds from now
         let tomorrow = Date.now.addingTimeInterval(86400)
         // create a range from those two
         let range = now...tomorrow
+        
+        
+//      a date that represented 8am today
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? .now
+//       Ask for the hour and minute, we’ll be handed back a DateComponents instance with optional values for all its properties.
+        let components2 = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components2.hour ?? 0
+        let minute = components2.minute ?? 0
+        
     }
 }
 
